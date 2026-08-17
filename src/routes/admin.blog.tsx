8689e657b15f46d0de1c970d82747f3cd3644d3a
Osuagwu101 <nnaemekasolomon31@gmail.com>
2026-08-17 18:36:03 +0100

    Gate admin blog layout

--- src/routes/admin.blog.tsx
+++ src/routes/admin.blog.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { requireAdminOrRedirect } from "@/lib/admin-gate";
 
 export const Route = createFileRoute("/admin/blog")({
+  ssr: false,
+  beforeLoad: async () => { await requireAdminOrRedirect(); },
   component: () => <Outlet />,
 });
